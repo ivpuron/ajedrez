39e9e626d8ee753cec1d00e24f37fe2521e6e578
ivpuron <ivpuron@unirioja.es>
2023-12-08 18:52:24 +0100

    tablero y movimiento de piezas sin reglas

--- Partida.java
+++ Partida.java
@@ -1,15 +1,20 @@
+import InterfazGrafica.Tablero;
+
 import java.net.Socket;
 
 public class Partida implements Runnable {
     private Socket player1;
     private Socket player2;
+    private boolean finalizada;
     public Partida(Socket player1, Socket player2) {
         this.player1=player1;
         this.player2=player2;
+        this.finalizada=false;
     }
 
     @Override
     public void run() {
+        Tablero tablero = new Tablero();
 
     }
 }
